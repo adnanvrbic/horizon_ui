9b6960f4042d4f352c62ef5ca03fd44d1fe876a2
VedranG99 <vedran,garasevic@gmail.com>
2025-07-02 03:29:27 +0200

    Add authentication flow on UI

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,26 @@ import { createApp } from 'vue'
 
 import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
+import { isAuthenticated, removeToken } from './store/auth'
+import { router } from './router'
 
 // Styles
 import '@core/scss/template/index.scss'
 import '@styles/styles.scss'
-
-// Create vue app
+  
+  setInterval(() => {
+    if (!isAuthenticated()) {
+      removeToken()
+      if (router.currentRoute.value.path !== '/login') {
+        router.push('/login')
+      }
+    }
+  }, 60000)
+  
 const app = createApp(App)
 
-// Register plugins
+app.use(router)
+
 registerPlugins(app)
 
-// Mount vue app
 app.mount('#app')
